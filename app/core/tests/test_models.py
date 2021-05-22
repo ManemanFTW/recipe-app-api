f8ce96cd6305c3a7092db5cb1587fb28ed659aef
Manuel Flores <manemanftw@outlook.com>
2021-05-22 15:53:57 -0500

    Updated admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -8,9 +9,9 @@ class ModelTests(TestCase):
         email = 'test@londonappdev.com'
         password = 'Password123'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -19,7 +20,7 @@ class ModelTests(TestCase):
         """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.com'
         user = get_user_model().objects.create_user(email, 'test123')
-        
+
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
